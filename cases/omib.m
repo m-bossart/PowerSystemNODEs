62c12ed86da9692bea540fcf686cc8ba35b59592
Matt <67015312+m-bossart@users.noreply.github.com>
2021-07-01 19:26:46 -0600

    add power system cases

---
+++ cases/omib.m
@@ -0,0 +1,37 @@
+function mpc = omib
+%   MATPOWER
+
+%% MATPOWER Case Format : Version 2
+mpc.version = '2';
+
+%%-----  Power Flow Data  -----%%
+%% system MVA base
+mpc.baseMVA = 100;
+
+%% bus data
+%	bus_i	type	Pd	Qd	Gs	Bs	area	Vm	Va	baseKV	zone	Vmax	Vmin
+mpc.bus = [
+	1	3	0	0	0	0	1	1	0	345	1	1.1	0.9;      %ref bus (IB)
+	2	2	90	30	0	0	1	1	0	345	1	1.1	0.9;  %generator bus
+
+];
+
+%% generator data
+%	bus	Pg	Qg	Qmax	Qmin	Vg	mBase	status	Pmax	Pmin	Pc1	Pc2	Qc1min	Qc1max	Qc2min	Qc2max	ramp_agc	ramp_10	ramp_30	ramp_q	apf
+mpc.gen = [
+	2	90	30	300	-300	1.025	100	1	300	10	0	0	0	0	0	0	0	0	0	0	0;
+];
+
+%% branch data
+%	fbus	tbus	r	x	b	rateA	rateB	rateC	ratio	angle	status	angmin	angmax
+mpc.branch = [
+	1	2	0.01	0.0576	0	250	250	250	0	0	1	-360	360;
+];
+
+%%-----  OPF Data  -----%%
+%% generator cost data
+%	1	startup	shutdown	n	x1	y1	...	xn	yn
+%	2	startup	shutdown	n	c(n-1)	...	c0
+mpc.gencost = [
+	2	2000	0	3	0.085	1.2	600;
+];
