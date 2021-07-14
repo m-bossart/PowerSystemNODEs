7daefc08870265a9a9fb7e9fa06bfd6401708fd9
Jose Daniel Lara <jdlara@berkeley.edu>
2021-07-14 12:30:58 -0700

    simplify code

--- models/init_functions.jl
+++ models/init_functions.jl
@@ -1,6 +1,6 @@
 include("InverterModels.jl")
 function get_init_gfm(p, vr_filter, vi_filter, ir_filter, ii_filter)
-    return function init_gfm(dx, x, p, vr_filter, vi_filter, ir_filter, ii_filter)
+    return  (dx, x) -> begin
             dx[1] = 0
             x[1] = vi_filter
             dx[10]
@@ -11,11 +11,11 @@ function get_init_gfm(p, vr_filter, vi_filter, ir_filter, ii_filter)
             x[19] = ii_filter
             gfm(dx,x,p,0)
             nothing
-            end
+        end
 end
 
 function get_init_gfm_mm(p, vr_filter, vi_filter, ir_filter, ii_filter)
-    return function init_gfm_nn_out(dx, x, p, vr_filter, vi_filter, ir_filter, ii_filter)
+    return (dx, x) -> begin
                 dx[1] = 0
                 x[1] = vi_filter
                 dx[10]
@@ -26,5 +26,5 @@ function get_init_gfm_mm(p, vr_filter, vi_filter, ir_filter, ii_filter)
                 x[19] = ii_filter
                 gfm_nn_out(dx,x,p,0)
                 nothing
-            end
+        end
 end
