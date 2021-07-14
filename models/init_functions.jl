29e052cd3264fd28786508ce2e05572a881b29a8
Jose Daniel Lara <jdlara@berkeley.edu>
2021-07-14 12:06:19 -0700

    add init_f getters

---
+++ models/init_functions.jl
@@ -0,0 +1,29 @@
+function get_init_gfm(p, vr_filter, vi_filter, ir_filter, ii_filter)
+    return function init_gfm(dx, x, p, vr_filter, vi_filter, ir_filter, ii_filter)
+            dx[1] = 0
+            x[1] = vi_filter
+            dx[10]
+            x[10] = vr_filter
+            dx[9] = 0
+            x[9] = ir_filter
+            dx[19]
+            x[19] = ii_filter
+            gfm(dx,x,p,0)
+            nothing
+            end
+end
+
+function get_init_gfm_mm(p, vr_filter, vi_filter, ir_filter, ii_filter)
+    return function init_gfm_nn_out(dx, x, p, vr_filter, vi_filter, ir_filter, ii_filter)
+                dx[1] = 0
+                x[1] = vi_filter
+                dx[10]
+                x[10] = vr_filter
+                dx[9] = 0
+                x[9] = ir_filter
+                dx[19]
+                x[19] = ii_filter
+                gfm_nn_out(dx,x,p,0)
+                nothing
+            end
+end
