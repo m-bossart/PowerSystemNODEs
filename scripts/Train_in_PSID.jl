0dacaead40d918815744e4be28f6f80b4f69460a
Matt <67015312+m-bossart@users.noreply.github.com>
2021-07-14 12:43:23 -0600

    start of training with PSID script

--- scripts/Train_in_PSID.jl
+++ scripts/Train_in_PSID.jl
@@ -1,18 +1,1 @@
 #PSEUDO CODE OUTLINING THE VARIOUS STEPS FOR TRAINING UODE.
-
-#Load a library of fault data in the form of Fourier coefficients
-    #Divide into training and test sets.
-
-#Build Full System + IB system in PSID. This is the source of ground truth data.
-#Randomly assign parameter values over a certain range.
-
-#Case A: Largest
-
-    #Evaluate loss over the full test set.
-
-
-
-#Write a surrogate model with GFM structure plus two additional states for the NODE current source at output.
-
-#Two stage training process.
-function initialize_surrogate()
