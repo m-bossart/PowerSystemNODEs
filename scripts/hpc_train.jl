46d7c8c691a86c28bc62f9892e97d1b1f9846b3c
Matt <67015312+m-bossart@users.noreply.github.com>
2021-12-07 13:49:41 -0700

    test hpc_train.jl on summit

--- scripts/hpc_train.jl
+++ scripts/hpc_train.jl
@@ -11,11 +11,21 @@ end
 
 test = [TestParams("test"), TestParams("test2")]
 
-hpc_params = SavioHPCTrain(;
+#=  hpc_params = SavioHPCTrain(;
     username = "jdlara",
     params_data = test,
     project_folder = "test",
     scratch_path = "/global/home/users/jdlara",
 )
 
-generate_train_files(hpc_params)
+generate_train_files(hpc_params)  =#
+
+hpc_params = SummitHPCTrain(;
+    username = "mabo4366",
+    params_data = test, 
+    project_folder = "test",
+    scratch_path = "/scratch/summit/mabo4366"
+)
+
+generate_train_files(hpc_params) 
+run_parallel_train(hpc_params)
