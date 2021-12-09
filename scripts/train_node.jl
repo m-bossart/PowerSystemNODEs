d7a52f4602de4f1a5ea4fe33c36843b9905c7938
Matt <67015312+m-bossart@users.noreply.github.com>
2021-12-08 18:38:16 -0700

    include before logging

--- scripts/train_node.jl
+++ scripts/train_node.jl
@@ -1,8 +1,7 @@
-configure_logging(console_level = Logging.Info)
-#configure_logging(;filename = "train_node.log")
-
 include("../src/PowerSystemNODEs.jl")
 include("../system_data/dynamic_components_data.jl")
+configure_logging(console_level = Logging.Info)
+#configure_logging(;filename = "train_node.log")
 
 sample_train_parameters = "input_data/sample_train_parameters.json"
 serialize(NODETrainParams(), "input_data/sample_train_parameters.json")
