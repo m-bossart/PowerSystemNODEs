24a73c2d1ac5245f3e6bf9e9c46d552e4c691351
Matt <67015312+m-bossart@users.noreply.github.com>
2022-01-03 13:32:03 -0700

    dummy script to test hpc happening in parallel

---
+++ scripts/hpc_timing_test.jl
@@ -0,0 +1,15 @@
+using Dates
+open(string("timing",rand()), "w") do io
+    print(io, now())
+    print(io, "\n")
+    sleep(10)
+    print(io, now())
+    print(io, "\n")
+    sleep(10)
+    print(io, now())
+    print(io, "\n")
+    sleep(10)
+    print(io, now())
+    print(io, "\n")
+    sleep(10)
+end 
