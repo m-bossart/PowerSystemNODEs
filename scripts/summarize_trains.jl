68bc9c6d0bdd9a73781e4884daee0822c6473efa
Matt <67015312+m-bossart@users.noreply.github.com>
2022-01-05 15:42:43 -0700

    include dependencies in summarize_trains.jl

--- scripts/summarize_trains.jl
+++ scripts/summarize_trains.jl
@@ -1,4 +1,7 @@
 #Script to run after a collection of training instances to provide high level results
+include("../src/PowerSystemNODEs.jl")
+include("../src/constants.jl")
+include("../src/HPCTrain.jl")
 
 p = visualize_summary(NODETrainParams().output_data_path)
 png(p, joinpath(NODETrainParams().output_data_path, "train_summary"))
