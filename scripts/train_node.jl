f479117acd6e6c6278ded8459fcb044a334221b6
Matt <67015312+m-bossart@users.noreply.github.com>
2022-01-07 15:18:22 -0700

    change naming

--- scripts/train_node.jl
+++ scripts/train_node.jl
@@ -4,10 +4,10 @@ include("../system_data/dynamic_components_data.jl")
 configure_logging(console_level = Logging.Info)
 #configure_logging(;filename = "train_node.log")
 
-sample_train_parameters = "input_data/sample_train_parameters.json"
+sample_train_parameters = "input_data/sample_parameters.json"
 p = NODETrainParams()
 p.verify_psid_node_off = false
-serialize(p, "input_data/sample_train_parameters.json")
+serialize(p, "input_data/sample_parameters.json")
 
 train_params_file = isempty(ARGS) ? sample_train_parameters : ARGS[1]
 train_params = NODETrainParams(train_params_file)
