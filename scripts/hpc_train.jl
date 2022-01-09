ad75637bf4bb49704be8f736f4341ba8aefe6200
Matt <67015312+m-bossart@users.noreply.github.com>
2022-01-09 15:23:29 -0700

    add variation in group sizes

--- scripts/hpc_train.jl
+++ scripts/hpc_train.jl
@@ -142,7 +142,7 @@ push!(
         maxiters = 20000,
         groupsize_steps = 10,
         ode_model = "none",
-        train_id = "10",
+        train_id = "11",
         node_width = 5,
         node_layers = 5,
         node_feedback_states = 3,
@@ -150,6 +150,307 @@ push!(
     ),
 )
 
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "12",
+        node_width = 2,
+        node_layers = 2,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "13",
+        node_width = 3,
+        node_layers = 2,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "14",
+        node_width = 4,
+        node_layers = 2,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "15",
+        node_width = 5,
+        node_layers = 2,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "16",
+        node_width = 2,
+        node_layers = 3,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "17",
+        node_width = 2,
+        node_layers = 4,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "18",
+        node_width = 2,
+        node_layers = 5,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "19",
+        node_width = 2,
+        node_layers = 2,
+        node_feedback_states = 1,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "20",
+        node_width = 2,
+        node_layers = 2,
+        node_feedback_states = 2,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "21",
+        node_width = 2,
+        node_layers = 2,
+        node_feedback_states = 3,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 5,
+        ode_model = "none",
+        train_id = "22",
+        node_width = 5,
+        node_layers = 5,
+        node_feedback_states = 3,
+        graphical_report_mode = 3,
+    ),
+)
+
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "23",
+        node_width = 2,
+        node_layers = 2,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "24",
+        node_width = 3,
+        node_layers = 2,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "25",
+        node_width = 4,
+        node_layers = 2,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "26",
+        node_width = 5,
+        node_layers = 2,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "27",
+        node_width = 2,
+        node_layers = 3,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "28",
+        node_width = 2,
+        node_layers = 4,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "29",
+        node_width = 2,
+        node_layers = 5,
+        node_feedback_states = 0,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "30",
+        node_width = 2,
+        node_layers = 2,
+        node_feedback_states = 1,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "31",
+        node_width = 2,
+        node_layers = 2,
+        node_feedback_states = 2,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "32",
+        node_width = 2,
+        node_layers = 2,
+        node_feedback_states = 3,
+        graphical_report_mode = 3,
+    ),
+)
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "33",
+        node_width = 5,
+        node_layers = 5,
+        node_feedback_states = 3,
+        graphical_report_mode = 3,
+    ),
+)
+
+push!(
+    params_data,
+    NODETrainParams(
+        maxiters = 20000,
+        groupsize_steps = 2 ,
+        ode_model = "none",
+        train_id = "33_repeat",
+        node_width = 5,
+        node_layers = 5,
+        node_feedback_states = 3,
+        graphical_report_mode = 3,
+    ),
+)
 #=
  hpc_params = SavioHPCTrain(;
     username = "jdlara",
