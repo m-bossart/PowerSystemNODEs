96c1f4888ff8b7d5a8cd90fc7ce7f6893303b1a3
Matt <67015312+m-bossart@users.noreply.github.com>
2022-01-26 14:15:27 -0700

    basic hpc_train script with one default

--- scripts/train_node.jl
+++ scripts/train_node.jl
@@ -1,3 +1,4 @@
+using Revise
 using PowerSimulationNODE
 using Logging
 include("../system_data/dynamic_components_data.jl")
@@ -11,5 +12,22 @@ p.verify_psid_node_off = false
 PowerSimulationNODE.serialize(p, "input_data/sample_parameters.json")
 
 train_params_file = isempty(ARGS) ? sample_train_parameters : ARGS[1]
-train_params = NODETrainParams(train_params_file)
-status = train(train_params)
+
+#train_params = NODETrainParams(train_params_file)
+#status = train(train_params)
+
+
+
+
+#FOR DEBUGGING -- DELETE BELOW
+train_params = NODETrainParams(
+    train_id = "1",
+    maxiters = 10,
+    training_groups = [(tspan = (0.0, 1.0), multiple_shoot_group_size = 10, multiple_shoot_continuity_term = 100.0, batching_sample_factor = 1.0)],
+    node_width = 40,
+    node_layers = 5,
+    node_activation = "relu",
+    graphical_report_mode = 3,
+)
+@time train(train_params)
+
