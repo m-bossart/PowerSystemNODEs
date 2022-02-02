730406b69ef4c9484cde844c50e518982677dfab
Matt <67015312+m-bossart@users.noreply.github.com>
2022-02-01 17:57:06 -0700

    function to build combinations of params

--- scripts/hpc_train.jl
+++ scripts/hpc_train.jl
@@ -1,151 +1,94 @@
 using PowerSimulationNODE
+using Base.Cartesian
+
+function build_params_list!(params_data, no_change_params, change_params)
+    train_id = 1
+    starting_dict = no_change_params
+    dims = []
+    for (k, v) in change_params
+        push!(dims, length(v))
+    end
+    dims_tuple = tuple(dims...)
+    iterator = CartesianIndices(dims_tuple)
+    for i in iterator
+        for (j, (key, value)) in enumerate(change_params)
+            starting_dict[key] = value[i[j]]
+        end
+        starting_dict[:train_id] = string(train_id)
+        push!(params_data, NODETrainParams(; starting_dict...))
+        starting_dict = no_change_params
+        train_id += 1
+    end
+end
 
 params_data = NODETrainParams[]
+no_change_params = Dict{Symbol, Any}()
+change_params = Dict{Symbol, Any}()
 
-push!(
-    params_data,
-    NODETrainParams(
-        train_id = "1",
-        maxiters = 2000,
-        training_groups = [(
-            tspan = (0.0, 1.0),
-            multiple_shoot_group_size = 10,
-            multiple_shoot_continuity_term = 100.0,
-            batching_sample_factor = 1.0,
-        )],
-        node_width = 5,
-        node_layers = 2,
-        node_activation = "relu",
-        graphical_report_mode = 3,
-    ),
-)
+#INDICATE CONSTANT, NON-DEFAULT PARAMETERS
+no_change_params[:maxiters] = 2000
+no_change_params[:node_width] = 20
+no_change_params[:node_layers] = 2
+no_change_params[:graphical_report_mode] = 3
 
-push!(
-    params_data,
-    NODETrainParams(
-        train_id = "2",
-        maxiters = 2000,
-        training_groups = [(
-            tspan = (0.0, 1.0),
-            multiple_shoot_group_size = 10,
-            multiple_shoot_continuity_term = 100.0,
-            batching_sample_factor = 1.0,
-        )],
-        node_width = 10,
-        node_layers = 2,
-        node_activation = "relu",
-        graphical_report_mode = 3,
-    ),
-)
+#INDICATE PARAMETES TO ITERATE OVER COMBINATORIALLY 
+change_params[:node_activation] = ["relu", "hardtanh", "sigmoid"]
+change_params[:training_groups] = [
+    [(
+        tspan = (0.0, 1.0),
+        multiple_shoot_group_size = 10,
+        multiple_shoot_continuity_term = 100,
+        batching_sample_factor = 1.0,
+    )],
+    [(
+        tspan = (0.0, 1.0),
+        multiple_shoot_group_size = 20,
+        multiple_shoot_continuity_term = 100,
+        batching_sample_factor = 1.0,
+    )],
+    [(
+        tspan = (0.0, 1.0),
+        multiple_shoot_group_size = 101,
+        multiple_shoot_continuity_term = 100,
+        batching_sample_factor = 1.0,
+    )],
+]
+change_params[:node_state_inputs] = [
+    [],
+    [("gen1", :ir_filter), ("gen1", :ii_filter)],
+    [("gen1", :q_oc), ("gen1", :θ_oc), ("gen1", :ω_oc)],
+    [
+        ("gen1", :γd_ic),
+        ("gen1", :γq_ic),
+        ("gen1", :ξd_ic),
+        ("gen1", :ξq_ic),
+        ("gen1", :ϕd_ic),
+        ("gen1", :ϕq_ic),
+    ],
+    [
+        ("gen1", :vi_filter),
+        ("gen1", :γd_ic),
+        ("gen1", :vq_pll),
+        ("gen1", :γq_ic),
+        ("gen1", :ξd_ic),
+        ("gen1", :ϕd_ic),
+        ("gen1", :ε_pll),
+        ("gen1", :ir_cnv),
+        ("gen1", :vr_filter),
+        ("gen1", :ω_oc),
+        ("gen1", :ξq_ic),
+        ("gen1", :vd_pll),
+        ("gen1", :q_oc),
+        ("gen1", :ϕq_ic),
+        ("gen1", :θ_pll),
+        ("gen1", :θ_oc),
+        ("gen1", :ii_cnv),
+    ],
+]
 
-push!(
-    params_data,
-    NODETrainParams(
-        train_id = "3",
-        maxiters = 2000,
-        training_groups = [(
-            tspan = (0.0, 1.0),
-            multiple_shoot_group_size = 10,
-            multiple_shoot_continuity_term = 100.0,
-            batching_sample_factor = 1.0,
-        )],
-        node_width = 15,
-        node_layers = 2,
-        node_activation = "relu",
-        graphical_report_mode = 3,
-    ),
-)
-
-push!(
-    params_data,
-    NODETrainParams(
-        train_id = "4",
-        maxiters = 2000,
-        training_groups = [(
-            tspan = (0.0, 1.0),
-            multiple_shoot_group_size = 10,
-            multiple_shoot_continuity_term = 100.0,
-            batching_sample_factor = 1.0,
-        )],
-        node_width = 20,
-        node_layers = 2,
-        node_activation = "relu",
-        graphical_report_mode = 3,
-    ),
-)
-
-push!(
-    params_data,
-    NODETrainParams(
-        train_id = "5",
-        maxiters = 2000,
-        training_groups = [(
-            tspan = (0.0, 1.0),
-            multiple_shoot_group_size = 10,
-            multiple_shoot_continuity_term = 100.0,
-            batching_sample_factor = 1.0,
-        )],
-        node_width = 5,
-        node_layers = 3,
-        node_activation = "relu",
-        graphical_report_mode = 3,
-    ),
-)
-
-push!(
-    params_data,
-    NODETrainParams(
-        train_id = "6",
-        maxiters = 2000,
-        training_groups = [(
-            tspan = (0.0, 1.0),
-            multiple_shoot_group_size = 10,
-            multiple_shoot_continuity_term = 100.0,
-            batching_sample_factor = 1.0,
-        )],
-        node_width = 10,
-        node_layers = 3,
-        node_activation = "relu",
-        graphical_report_mode = 3,
-    ),
-)
-
-push!(
-    params_data,
-    NODETrainParams(
-        train_id = "7",
-        maxiters = 2000,
-        training_groups = [(
-            tspan = (0.0, 1.0),
-            multiple_shoot_group_size = 10,
-            multiple_shoot_continuity_term = 100.0,
-            batching_sample_factor = 1.0,
-        )],
-        node_width = 15,
-        node_layers = 3,
-        node_activation = "relu",
-        graphical_report_mode = 3,
-    ),
-)
-
-push!(
-    params_data,
-    NODETrainParams(
-        train_id = "8",
-        maxiters = 2000,
-        training_groups = [(
-            tspan = (0.0, 1.0),
-            multiple_shoot_group_size = 10,
-            multiple_shoot_continuity_term = 100.0,
-            batching_sample_factor = 1.0,
-        )],
-        node_width = 20,
-        node_layers = 3,
-        node_activation = "relu",
-        graphical_report_mode = 3,
-    ),
-)
-
+build_params_list!(params_data, no_change_params, change_params)
+@warn length(params_data)
+##
 #=
  hpc_params = SavioHPCTrain(;
     username = "jdlara",
@@ -160,7 +103,7 @@ hpc_params = SummitHPCTrain(;
     params_data = params_data,
     project_folder = "PowerSystemNODEs",
     scratch_path = "/scratch/summit/mabo4366",
-    n_tasks = 8,
+    n_tasks = length(params_data),
     force_generate_inputs = true,
 )
 
