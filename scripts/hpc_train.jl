281e683791f62aaf3c7d9222e7c14e43079d7e6f
m-bossart <bossart.matt@gmail.com>
2022-06-16 08:41:18 -0600

    update new hpctrain params

--- scripts/hpc_train.jl
+++ scripts/hpc_train.jl
@@ -46,17 +46,17 @@ build_params_list!(params_data, no_change_params, change_params)
 
 hpc_params = SummitHPCTrain(;
     username = "mabo4366",
-    params_data = params_data,  #have the default time be infinite...
+    params_data = params_data,
     project_folder = "PowerSystemNODEs",
     scratch_path = "/scratch/summit/mabo4366",
-    n_tasks = length(params_data),
-    time_limit = "23:59:59",        #add a time parameter which allocates (say 30 mins) to generating data, getting things instantiated, etc. 
+    time_limit_train = "23:59:59",       
+    time_limit_generate_data = "00:30:00", 
     QoS = "normal",
     partition = "shas", #"shas-testing"
     force_generate_inputs = true,
     mb_per_cpu = 4800,
 )
 
-generate_train_files(hpc_params)    #when you generate the train data, first re-set the time limit in params before serializing it... 
-##                                     #TODO - actually implement the time limit for training.
+generate_train_files(hpc_params)   
+##                                   
 run_parallel_train(hpc_params)
