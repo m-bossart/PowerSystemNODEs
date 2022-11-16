0cb9b0236f75ae456bb05d9a760ed607d7ceadb3
m-bossart <bossart.matt@gmail.com>
2022-11-16 12:49:28 -0500

    new experiment

--- scripts/analyze/visualize_trains.jl
+++ scripts/analyze/visualize_trains.jl
@@ -3,7 +3,7 @@ using PowerSimulationNODE
 using Plots
 using Logging
 #include("../system_data/dynamic_components_data.jl")
-train_folder = joinpath("transfers", "exp_11_09_22")
+train_folder = joinpath("transfers", "exp_11_14_22")
 configure_logging(console_level = Logging.Info)
 
 visualize_level = isempty(ARGS) ? 3 : parse(Int64, ARGS[1])
@@ -34,6 +34,6 @@ train_files_with_output = filter(
 ##
 
 for file in train_files_with_output
-    visualize_training(file, skip = 10, new_base_path = train_folder)       #re-base path should be separate
+    visualize_training(file, skip = 1, new_base_path = train_folder)       #TODO - re-base path should be separate
     #animate_training(file, skip = 100)
 end
--- scripts/hpc_train/hpc_train.jl
+++ scripts/hpc_train/hpc_train.jl
@@ -1,25 +1,29 @@
 using PowerSimulationNODE
 using PowerSimulationsDynamicsSurrogates
 const PSIDS = PowerSimulationsDynamicsSurrogates
-
+if Sys.iswindows() || Sys.isapple()
+    const SCRATCH_PATH = joinpath(pwd(), "..")
+else
+    const SCRATCH_PATH = "/scratch/alpine/mabo4366"
+end
 train_folder = "exp_1"    #The name of the folder where everything related to the group of trainings will be stored (inputs, outputs, systems, logging, etc.)
 system_name = "CTESN_18bus_modified" #The specific system from the "systems" folder to use. Will be copied over to the train_folder to make it self-contained.
 project_folder = "PowerSystemNODEs"
-scratch_path = "/scratch/alpine/mabo4366"  #Options: [ joinpath(pwd(), ".."), "/scratch/alpine/mabo4366"]
+#scratch_path = "/scratch/alpine/mabo4366"  #Options: [ joinpath(pwd(), ".."), "/scratch/alpine/mabo4366"]
 
 #Copy the full system over to the training directory.
 mkpath(
     joinpath(
-        scratch_path,
+        SCRATCH_PATH,
         project_folder,
         train_folder,
         PowerSimulationNODE.INPUT_SYSTEM_FOLDER_NAME,
     ),
 )
 cp(
-    joinpath(scratch_path, project_folder, "systems", string(system_name, ".json")),
+    joinpath(SCRATCH_PATH, project_folder, "systems", string(system_name, ".json")),
     joinpath(
-        scratch_path,
+        SCRATCH_PATH,
         project_folder,
         train_folder,
         PowerSimulationNODE.INPUT_SYSTEM_FOLDER_NAME,
@@ -29,13 +33,13 @@ cp(
 )
 cp(
     joinpath(
-        scratch_path,
+        SCRATCH_PATH,
         project_folder,
         "systems",
         string(system_name, "_validation_descriptors.json"),
     ),
     joinpath(
-        scratch_path,
+        SCRATCH_PATH,
         project_folder,
         train_folder,
         PowerSimulationNODE.INPUT_SYSTEM_FOLDER_NAME,
@@ -50,7 +54,7 @@ change_params = Dict{Symbol, Any}()
 
 #INDICATE CONSTANT, NON-DEFAULT PARAMETERS (surrogate_buses and system_path CANNOT change; train_id set automatically)
 no_change_params[:surrogate_buses] = [20]
-no_change_params[:train_data] = (
+#= no_change_params[:train_data] = (
     id = "1",
     operating_points = PSIDS.SurrogateOperatingPoint[PSIDS.GenerationLoadScale(
         generation_scale = 1.0,
@@ -72,7 +76,7 @@ no_change_params[:train_data] = (
         seed = 1,
     ),
     system = "full",
-)
+) =#
 no_change_params[:validation_data] = (
     id = "1",
     operating_points = PSIDS.SurrogateOperatingPoint[PSIDS.GenerationLoadScale(
@@ -120,13 +124,13 @@ no_change_params[:test_data] = (
 no_change_params[:hidden_states] = 5
 no_change_params[:model_initializer] =
     (type = "dense", n_layer = 2, width_layers = 10, activation = "hardtanh")
-no_change_params[:model_node] = (
+#= no_change_params[:model_node] = (
     type = "dense",
     n_layer = 1,
     width_layers = 10,
     activation = "hardtanh",
     σ2_initialization = 0.0,
-)
+) =#
 no_change_params[:model_observation] =
     (type = "dense", n_layer = 1, width_layers = 10, activation = "hardtanh")
 no_change_params[:scaling_limits] =
@@ -135,16 +139,21 @@ no_change_params[:steady_state_solver] =
     (solver = "SSRootfind", abstol = 1e-4, maxiters = 5)
 no_change_params[:dynamic_solver] =
     (solver = "Rodas5", reltol = 1e-3, abstol = 1e-6, maxiters = 1e5)
-#= no_change_params[:optimizer] = (
+no_change_params[:optimizer] = (
     sensealg = "Zygote",
     primary = "Adam",
-    primary_η = 0.0001,
-    adjust = "nothing",
-    adjust_η = 0.0,
-) =#
+    primary_η = 0.01,
+    primary_maxiters = 3000,
+    adjust = "Bfgs",
+    adjust_initial_stepnorm = 0.00001,
+    adjust_maxiters = 300,
+)
 no_change_params[:lb_loss] = 0.0
-no_change_params[:curriculum] = "none"
-no_change_params[:curriculum_timespans] =
+#no_change_params[:primary_curriculum] = "individual faults"
+no_change_params[:primary_curriculum_timespans] =
+    [(tspan = (0.0, 10.0), batching_sample_factor = 1.0)]
+#no_change_params[:adjust_curriculum] = "simultaneous"
+no_change_params[:adjust_curriculum_timespans] =
     [(tspan = (0.0, 10.0), batching_sample_factor = 1.0)]
 no_change_params[:validation_loss_every_n] = 100
 no_change_params[:loss_function] = (
@@ -154,13 +163,13 @@ no_change_params[:loss_function] = (
         residual_penalty = 1.0,
     ),
     type_weights = (rmse = 1.0, mae = 0.0),
-) 
+)
 no_change_params[:rng_seed] = 123
 no_change_params[:output_mode_skip] = 1
 no_change_params[:train_time_limit_seconds] = 1e9
-no_change_params[:base_path] = joinpath(scratch_path, project_folder, train_folder)
+no_change_params[:base_path] = joinpath(SCRATCH_PATH, project_folder, train_folder)
 no_change_params[:system_path] = joinpath(
-    scratch_path,
+    SCRATCH_PATH,
     project_folder,
     train_folder,
     PowerSimulationNODE.INPUT_SYSTEM_FOLDER_NAME,
@@ -180,45 +189,96 @@ no_change_params[:system_path] = joinpath(
     ),
 ]
  =#
-change_params[:optimizer] = [
+change_params[:model_node] = [
     (
-        sensealg = "Zygote",
-        primary = "Adam",
-        primary_η = 1.0,
-        primary_maxiters = 5000,
-        adjust = "Bfgs",
-        adjust_initial_stepnorm = 0.00001,
-        adjust_maxiters = 50,
+        type = "dense",
+        n_layer = 1,
+        width_layers = 10,
+        activation = "hardtanh",
+        σ2_initialization = 0.0,
     ),
     (
-        sensealg = "Zygote",
-        primary = "Adam",
-        primary_η = 0.1,
-        primary_maxiters = 5000,
-        adjust = "Bfgs",
-        adjust_initial_stepnorm = 0.00001,
-        adjust_maxiters = 50,
-    ),
-    (
-        sensealg = "Zygote",
-        primary = "Adam",
-        primary_η = 0.01,
-        primary_maxiters = 5000,
-        adjust = "Bfgs",
-        adjust_initial_stepnorm = 0.00001,
-        adjust_maxiters = 50,
-    ),
-    (
-        sensealg = "Zygote",
-        primary = "Adam",
-        primary_η = 0.001,
-        primary_maxiters = 5000,
-        adjust = "Bfgs",
-        adjust_initial_stepnorm = 0.00001,
-        adjust_maxiters = 50,
+        type = "dense",
+        n_layer = 2,
+        width_layers = 10,
+        activation = "hardtanh",
+        σ2_initialization = 0.0,
     ),
 ]
-
+change_params[:primary_curriculum] =
+    ["individual faults", "individual faults x2", "individual faults x3"]
+change_params[:adjust_curriculum] = ["individual faults", "simultaneous"]
+change_params[:train_data] = [
+    (
+        id = "1",
+        operating_points = PSIDS.SurrogateOperatingPoint[PSIDS.GenerationLoadScale(
+            generation_scale = 1.0,
+            load_scale = 1.0,
+        ),],
+        perturbations = repeat(
+            [[PSIDS.RandomLoadChange(time = 1.0, load_multiplier_range = (0.0, 2.0))]],
+            5,
+        ),
+        params = PSIDS.GenerateDataParams(
+            solver = "Rodas5",
+            solver_tols = (reltol = 1e-3, abstol = 1e-6),
+            tspan = (0.0, 10.0),
+            tstops = 0.0:0.1:10.0,
+            tsave = 0.0:0.1:10.0,
+            formulation = "MassMatrix",
+            all_branches_dynamic = false,
+            all_lines_dynamic = true,
+            seed = 1,
+        ),
+        system = "full",
+    ),
+    (
+        id = "2",
+        operating_points = PSIDS.SurrogateOperatingPoint[PSIDS.GenerationLoadScale(
+            generation_scale = 1.0,
+            load_scale = 1.0,
+        ),],
+        perturbations = repeat(
+            [[PSIDS.RandomLoadChange(time = 1.0, load_multiplier_range = (0.0, 2.0))]],
+            5,
+        ),
+        params = PSIDS.GenerateDataParams(
+            solver = "Rodas5",
+            solver_tols = (reltol = 1e-3, abstol = 1e-6),
+            tspan = (0.0, 10.0),
+            tstops = 0.0:0.1:10.0,
+            tsave = 0.0:0.1:10.0,
+            formulation = "MassMatrix",
+            all_branches_dynamic = false,
+            all_lines_dynamic = true,
+            seed = 2,
+        ),
+        system = "full",
+    ),
+    (
+        id = "3",
+        operating_points = PSIDS.SurrogateOperatingPoint[PSIDS.GenerationLoadScale(
+            generation_scale = 1.0,
+            load_scale = 1.0,
+        ),],
+        perturbations = repeat(
+            [[PSIDS.RandomLoadChange(time = 1.0, load_multiplier_range = (0.0, 2.0))]],
+            5,
+        ),
+        params = PSIDS.GenerateDataParams(
+            solver = "Rodas5",
+            solver_tols = (reltol = 1e-3, abstol = 1e-6),
+            tspan = (0.0, 10.0),
+            tstops = 0.0:0.1:10.0,
+            tsave = 0.0:0.1:10.0,
+            formulation = "MassMatrix",
+            all_branches_dynamic = false,
+            all_lines_dynamic = true,
+            seed = 3,
+        ),
+        system = "full",
+    ),
+]
 build_params_list!(params_data, no_change_params, change_params)
 @warn "Number of trainings:", length(params_data)
 ##
@@ -236,7 +296,7 @@ hpc_params = AlpineHPCTrain(;
     params_data = params_data,
     project_folder = project_folder,
     train_folder = train_folder,
-    scratch_path = scratch_path,
+    scratch_path = SCRATCH_PATH,
     time_limit_train = "11:59:59",             #Options: ["00:30:00", "23:59:59"]
     time_limit_generate_data = "01:00:00",
     QoS = "normal",
